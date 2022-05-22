5f1453d371a34d99d1c33cc350f75eec0ac2f075
Melanie <melanie16203@gmail.com>
2022-05-22 01:24:00 -0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import SEO from 'components/SEO'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 import 'styles/globals.css'
@@ -5,6 +6,7 @@ import 'styles/globals.css'
 export default function BlogSite({ Component, pageProps }) {
     return (
         <>
+            <SEO />
             <Header />
             <main className='py-12'>
                 <Component {...pageProps} />
