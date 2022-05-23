a56d8bcede35705680c3255b9b23435abb40c9e4
Melanie <melanie16203@gmail.com>
2022-05-23 04:14:54 -0300

    Authors CMS integrated

--- pages/autores.js
+++ pages/autores.js
@@ -1,4 +1,6 @@
-export default function Authors() {
+import axiosClient from 'config/axios'
+
+export default function Authors({ authors }) {
     return (
         <div className='container text-center pb-5'>
             <span className='text-lg font-bold text-blue-700 dark:text-blue-400'>
@@ -6,35 +8,32 @@ export default function Authors() {
             </span>
             <h1 className='text-3xl font-bold'>¿Quiénes somos?</h1>
 
-            <div className='flex justify-center gap-24 pt-16'>
-                <div className='max-w-[17rem]'>
-                    <img
-                        src='/avatar_redex.png'
-                        alt='LaParca80'
-                        className='rounded-full w-[300px] h-[275px] shadow hover:shadow-lg hover:-translate-y-1 transition-all duration-300 object-cover'
-                    />
-                    <h2 className='text-2xl font-bold my-5'>LaParca80</h2>
-                    <p>
-                        Soy un desarrollador web de Florida, con experiencia en
-                        frontend y backend. He construido desde simples páginas
-                        estáticas hasta un clon de Twitter. Trabajo actualmente
-                        con React y Node.js.
-                    </p>
-                </div>
-                <div className='max-w-[17rem]'>
-                    <img
-                        src='/avatar_redex.png'
-                        alt='redeX'
-                        className='rounded-full w-[300px] h-[275px] shadow dark:shadow-none hover:shadow-lg hover:-translate-y-1 transition-all duration-300 object-cover'
-                    />
-                    <h2 className='text-2xl font-bold my-5'>redeX</h2>
-                    <p>
-                        Soy un desarrollador web de Uruguay, con experiencia en
-                        la creación de sitios estáticos y dinámicos. Tengo
-                        conocimiento en HTML, CSS, JavaScript.
-                    </p>
-                </div>
+            <div className='flex flex-wrap justify-center gap-x-24 gap-y-10 pt-16'>
+                {authors.map(({ id, attributes: author }) => (
+                    <div key={id} className='max-w-[17rem]'>
+                        <img
+                            src={author.image.data.attributes.url}
+                            alt={author.name}
+                            className='rounded-full w-[300px] h-[275px] shadow hover:shadow-lg hover:-translate-y-1 transition-all duration-300 object-cover'
+                        />
+                        <h2 className='text-2xl font-bold my-5'>
+                            {author.name}
+                        </h2>
+                        <p>{author.about}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const { data } = await axiosClient.get('/authors?populate=*')
+
+    return {
+        props: {
+            authors: data.data,
+        },
+        revalidate: 10,
+    }
 }
